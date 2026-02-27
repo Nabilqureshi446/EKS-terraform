11e5640cf81ae35c4de2bc7a6b016657173bffa3
[Nabil Qureshi] <[nabilqureshi446@gmail.com]>
2026-02-27 19:26:27 +0530

    fix

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -84,7 +84,7 @@ resource "aws_iam_policy_attachment" "node_policy_attachment_ec2" {
   
 }
 
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my_node_group"
   node_role_arn   = aws_iam_role.node_role.arn
